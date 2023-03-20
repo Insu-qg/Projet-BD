ee68c2d5cf7f8b0d343c86b873ff261c75738ed8
l3miage-damessis <122186716+l3miage-damessis@users.noreply.github.com>
2023-03-20 17:11:37 +0100

    Create CadreRepository.java

---
+++ l3-miage-project-bd-main/l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/repo/CadreRepository.java
@@ -0,0 +1,41 @@
+package fr.uga.l3miage.photonum.data.repo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import fr.uga.l3miage.photonum.data.domain.Cadre;
+import fr.uga.l3miage.photonum.data.domain.Tirage;
+import jakarta.persistence.EntityManager;
+
+public class CadreRepository implements CRUDRepository<Long,Cadre> {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public CadreRepository(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+    @Override
+    public Cadre save(Cadre cadre) {
+        entityManager.persist(cadre);
+        return cadre;
+    }
+
+    @Override
+    public Cadre get(Long id) {
+        return entityManager.find(Cadre.class, id);
+    }
+
+    @Override
+    public void delete(Cadre cadre) {
+        entityManager.remove(cadre);
+    }
+
+    @Override
+    public List<Cadre> all() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'all'");
+    }
+    
+}
