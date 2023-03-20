f3c8a3127fac0e7c721dd9026964b35c269a2bda
l3miage-grangeq <quent.grange@gmail.com>
2023-03-20 17:36:18 +0100

    maj domain

--- l3-miage-project-bd-main/l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/domain/Exemplaire.java
+++ l3-miage-project-bd-main/l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/domain/Exemplaire.java
@@ -2,6 +2,7 @@ package fr.uga.l3miage.photonum.data.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -11,5 +12,10 @@ public class Exemplaire {
     @Column(name = "quantite")
     private int quantite;
 
-    
+    @ManyToOne
+    private Photo photo;
+
+    @ManyToOne
+    private Tirage tirage;
+
 }
