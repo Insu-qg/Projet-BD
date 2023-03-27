c71f95e9db321f0b2c52b04fa1d0fd3336331c77
l3miage-condese <Seynabou.Conde@etu.univ-grenoble-alpes.fr>
2023-03-27 18:13:07 +0200

    modif

---
+++ l3-miage-project-bd-main/l3-miage-project-bd-main/service-pub/src/main/java/fr/uga/l3miage/photonum/service/AlbumService.java
@@ -0,0 +1,24 @@
+package fr.uga.l3miage.photonum.service;
+
+import fr.uga.l3miage.photonum.data.domain.Album;
+import fr.uga.l3miage.photonum.service.base.BaseService;
+
+public interface AlbumService extends BaseService<Album, Long> {
+    /**
+     * Saves an album object
+     *
+     * @param album to be saved
+     * @return the album with an id
+     */
+    Album save(Album album);
+
+    /**
+     * Deletes an album
+     *
+     * @param id id of the album to delete
+     * @throws EntityNotFoundException when the entity do not already exists
+     * @throws DeleteException   when an album has books that are co-authored
+     */
+    void delete(Long id) throws EntityNotFoundException, DeleteException;
+
+}
