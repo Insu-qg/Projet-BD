5c04f423f00be37794788ae1f8eb61d581d74d8f
l3miage-damessis <122186716+l3miage-damessis@users.noreply.github.com>
2023-03-27 13:46:06 +0200

    1

--- l3-miage-project-bd-main/l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/repo/TirageRepository.java
+++ l3-miage-project-bd-main/l3-miage-project-bd-main/data/src/main/java/fr/uga/l3miage/photonum/data/repo/TirageRepository.java
@@ -36,6 +36,9 @@ public class TirageRepository implements CRUDRepository<Long,Tirage> {
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'all'");
     }
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
     
 }
