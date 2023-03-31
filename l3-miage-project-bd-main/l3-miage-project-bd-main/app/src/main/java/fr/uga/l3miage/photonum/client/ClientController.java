c12ef177e91ebb91bd3ebd8e1dfa27b369267171
l3miage-Sidibem <mariam.sidibe@etu.univ-grenoble-alpes.fr>
2023-03-31 10:48:56 +0200

    teste comit

--- l3-miage-project-bd-main/l3-miage-project-bd-main/app/src/main/java/fr/uga/l3miage/photonum/client/ClientController.java
+++ l3-miage-project-bd-main/l3-miage-project-bd-main/app/src/main/java/fr/uga/l3miage/photonum/client/ClientController.java
@@ -1,4 +1,9 @@
 package fr.uga.l3miage.photonum.client;
+
+import fr.uga.l3miage.photonum.album.AlbumMapper;
+import fr.uga.l3miage.photonum.cadre.CadreMapper;
+import fr.uga.l3miage.photonum.calendrier.CalendrierMapper;
+import fr.uga.l3miage.photonum.tirage.TirageMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -6,5 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/api/v1", produces = "application/json")
 public class ClientController {
 
-
+    private ClientMapper clientMapper;
+    private TirageMapper tirageMapper;
+    private AlbumMapper albumMapper;
+    private CadreMapper cadreMapper;
+    private CalendrierMapper calendrierMapper;
 }
