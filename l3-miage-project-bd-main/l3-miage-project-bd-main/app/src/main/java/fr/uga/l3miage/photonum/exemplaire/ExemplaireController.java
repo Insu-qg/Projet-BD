69324264719b16682669df5d46f9433b0c21d974
l3miage-Sidibem <mariam.sidibe@etu.univ-grenoble-alpes.fr>
2023-04-23 21:31:36 +0200

    test commit

--- l3-miage-project-bd-main/l3-miage-project-bd-main/app/src/main/java/fr/uga/l3miage/photonum/exemplaire/ExemplaireController.java
+++
@@ -1,9 +0,0 @@
-package fr.uga.l3miage.photonum.exemplaire;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping(value = "/api/v1", produces = "application/json")
-public class ExemplaireController {
-    
-}
